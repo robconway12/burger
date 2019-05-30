cffe3c9c1a82b92ac8b9bbf47d411c84c975ba1c
Rob Conway <robconway12@gmail.com>
2019-05-29 21:53:53 -0700

    Set up all files and directories

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
+INSERT INTO burgers (burger_name) VALUES ('Hawaiian Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Slider');
