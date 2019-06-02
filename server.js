fb555edc7d8525303af08a2e9784f502c2be0dd4
Rob Conway <robconway12@gmail.com>
2019-06-01 21:08:34 -0700

    completed server.js and required modules

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-var express = require("express");
-var PORT = process.env.PORT || 8080;
-var app = express();
+const express = require("express");
+const handlebars = require('express-handlebars')
+const PORT = process.env.PORT || 8080;
+const app = express();
+const bodyParser = require('body-parser');
+const override = require('method-override');
+
+app.use(express.static(_dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(override('_method'));
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT);
